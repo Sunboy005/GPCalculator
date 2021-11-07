0d0d437a0e436e6ffdbfa39832adbdf109b938cf
Amos Sunmbo Alagbe <sunboy005@gmail.com>
2021-11-07 15:21:00 +0100

    Course Services

---
+++ AppLibrary/Services/Implementations/CourseServices.cs
@@ -0,0 +1,28 @@
+﻿using AppLibrary.Services.DTOs;
+using AppModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace AppLibrary.Services.Implementations
+{
+    public class CourseServices
+    {
+        public bool Save(CoursesToAdd  model)
+        {
+            var id=  Guid.NewGuid().ToString();
+            var course = new Course(id, model.CourseName, model.CourseCode, model.CourseUnit);
+
+            return false;
+        }
+
+        public void ListCourses()
+        {
+            Console.WriteLine("Fetching Courses...");   // all users will be fetched
+            Thread.Sleep(1000);
+
+        }
+
+    }
+}
--- AppLibrary/Services/Interfaces/ICourse.cs
+++ AppLibrary/Services/Interfaces/ICourse.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AppLibrary.Services.Interface
+namespace AppLibrary.Services.Interfaces
 {
     interface Interface1
     {
